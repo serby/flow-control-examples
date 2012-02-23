1f30802d66f7247d267a7dbb881572573a1f2b44
Paul Serby <paul.serby@clock.co.uk>
2012-02-23 18:01:42 +0000

    Using jangle

--- conditional-contiunation/methods/jangle.js
+++ conditional-contiunation/methods/jangle.js
@@ -1,13 +1,13 @@
-var 
+var
   userModel = require('../lib/userModel').createUserModel(),
-  chain = require('../lib/flowy').chain;
+  chain = require('jangle').jangle;
 
 module.exports.findMe = function(user, callback){
 
   chain()
-  .link(userModel.findByName, user.name)
-  .link(userModel.findByEmail, user.email)
-  .link(userModel.findByGithub, user.github)
+  .seq(userModel.findByName, user.name)
+  .seq(userModel.findByEmail, user.email)
+  .seq(userModel.findByGithub, user.github)
   .detect(function(error, returnedUser) {
     return returnedUser;
   })
